6593fdac61a30a1c58bcd587ba9708fee2abd90d
codetivated <168292583+codetivated@users.noreply.github.com>
2025-12-02 17:23:40 -0600

    example to lazy load a service

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -5,28 +5,38 @@ import {
   canLeaveEditPage,
 } from '../tasks/new-task/new-task.component';
 import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    // loadComponent: () =>
-    //   import('../tasks/tasks.component').then(
-    //     (module) => module.TasksComponent
-    //   ),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () =>
+        //   import('../tasks/tasks.component').then(
+        //     (module) => module.TasksComponent
+        //   ),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
+
+// here we are lazy loading the TasksService for the /users/:userId route and its children routes
+// this is done because these components are the only ones that need the TasksService
